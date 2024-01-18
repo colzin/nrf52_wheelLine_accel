69fd5293d7408abecad55cb3f6b1b6a6e92ccc43
colzin <colzinmoore@gmail.com>
2024-01-18 11:58:24 -0700

    Removed build folder Debug

--- nRF52_wheelLine_accel_SDK17/wheelLineRemote_FW/Debug/nRF52_common/segger_RTT_v788j/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,6 +0,0 @@
-nRF52_common/segger_RTT_v788j/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- D:/Users/Collin/Documents/Wheel\ line\ movers/nRF52_wheelLine_accel_SDK17/nRF52_common/segger_RTT_v788j/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- d:\users\collin\documents\wheel\ line\ movers\nrf52_wheelline_accel_sdk17\nrf52_common\segger_rtt_v788j\rtt\segger_rtt.h \
- d:\users\collin\documents\wheel\ line\ movers\nrf52_wheelline_accel_sdk17\nrf52_common\segger_rtt_v788j\config\segger_rtt_conf.h
-d:\users\collin\documents\wheel\ line\ movers\nrf52_wheelline_accel_sdk17\nrf52_common\segger_rtt_v788j\rtt\segger_rtt.h:
-d:\users\collin\documents\wheel\ line\ movers\nrf52_wheelline_accel_sdk17\nrf52_common\segger_rtt_v788j\config\segger_rtt_conf.h:
